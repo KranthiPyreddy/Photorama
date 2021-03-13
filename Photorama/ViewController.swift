327e19d63de6349d5a11f2ea94e30424dc74f1fe
kranthi pyreddy <kra330@gmail.com>
2021-03-12 23:51:47 -0500

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Kranthi Pyreddy on 3/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
